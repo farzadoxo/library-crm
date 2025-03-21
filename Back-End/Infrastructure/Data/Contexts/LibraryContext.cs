707f949e6a1f06d306f0395dbac49a0dc0a15a97
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-03-21 20:21:13 +0330

    - main contex added

---
+++ Back-End/Infrastructure/Data/Contexts/LibraryContext.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data.Entities;
+
+namespace Infrastructure.Data.Contexts
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Auther> Authers { get; set; }
+        public DbSet<BookTopic> BookTopics { get; set; }
+        public DbSet<Lend> Lends { get; set; }
+        public DbSet<Librarian> Librarians { get; set; }
+        public DbSet<Library> Libraries { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+    }
+}
