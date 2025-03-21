7dbaf98ea9806b57a657078d2a62be10b6d2b0bc
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-03-21 20:20:56 +0330

    - some service configged (context configed)

--- Back-End/Program.cs
+++ Back-End/Program.cs
@@ -1,14 +1,18 @@
+using Infrastructure.Data.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+builder.Services.AddDbContext<LibraryContext>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
     app.UseSwaggerUI();
     app.MapControllers();
 }
