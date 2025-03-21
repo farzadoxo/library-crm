28a08fae12e54810886783a30ad0efe45456d591
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-03-21 20:21:38 +0330

    - dtos in progres ..

---
+++ Back-End/Application/DTOs/Book/AddBookDTO.cs
@@ -0,0 +1,16 @@
+using Domain.Entities;
+
+namespace Application.DTOs.Book
+{
+    public class AddBookDTO
+    {
+        public string? Title { get; set; }
+        public List<Auther>? Authers { get; set; }
+        public DateTime PublishDate { get; set; }
+        public Publisher? Publisher { get; set; }
+        public decimal Price { get; set; }
+        public double Edition { get; set; }
+        public int Tiraj { get; set; }
+        public BookTopic? Topic { get; set; }
+    }
+}
