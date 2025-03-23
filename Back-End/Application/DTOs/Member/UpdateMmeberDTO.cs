d97f3d6311dda93446889f84b6541df0a7338d18
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-03-23 11:38:41 +0330

    - dtos updated

--- Back-End/Application/DTOs/Member/UpdateMmeberDTO.cs
+++ Back-End/Application/DTOs/Member/UpdateMmeberDTO.cs
@@ -2,6 +2,7 @@ namespace Application.DTOs.Member
 {
     public class UpdateMemberDTO
     {
-
+        public string? FullName { get; set; }
+        public DateTime BirthDate { get; set; }
     }
 }
