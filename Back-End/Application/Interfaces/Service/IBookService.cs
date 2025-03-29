f4b37a30dd3a2c8297160471ba4a97e2e0d9fc86
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-03-29 18:03:11 +0330

    - services updated

--- Back-End/Application/Interfaces/Service/IBookService.cs
+++ Back-End/Application/Interfaces/Service/IBookService.cs
@@ -1,4 +1,5 @@
 using Application.Common;
+using Application.DTOs.Book;
 using Domain.Entities;
 
 namespace Application.Interfaces.Service
@@ -7,6 +8,14 @@ namespace Application.Interfaces.Service
     {
         Task<List<Book>> GetBooks(int take, int skip);
         Task<BookOprationResult> FindBook(int bookId);
+        Task<IQueryable> FindBooksByAuther(int autherId);
+        Task<IQueryable> FindBooksByTopic(int topicId);
+        Task<IQueryable> FindBooksByPublisher(int publisherId);
+        Task<BookOprationResult> AddBook(AddBookDTO dto);
+        Task<BookOprationResult> UpdateBook(int bookId, UpdateBookDTO dto);
+        Task<BookOprationResult> DeleteBook(int bookId);
+        Task<BookOprationResult> DeactiveBook(int bookId);
+        Task<BookOprationResult> ActiveBook(int bookId);
 
     }
 }
--- Back-End/Infrastructure/Service/BookService.cs
+++ Back-End/Infrastructure/Service/BookService.cs
@@ -1,4 +1,5 @@
 using Application.Common;
+using Application.DTOs.Book;
 using Application.Interfaces.Repository;
 using Application.Interfaces.Service;
 using Domain.Entities;
@@ -31,5 +32,75 @@ namespace Infrastructure.Service
             var books = _repo.GetBooks(skip:skip,take:take);
             return books;
         }
+        public async Task<IQueryable> FindBooksByAuther(int autherId)
+        {
+            var books = _repo.FindBooksByAuther(autherId);
+            return books;
+        }
+        public async Task<IQueryable> FindBooksByTopic(int topicId)
+        {
+            var books = _repo.FindBooksByTopic(topicId);
+            return books;
+        }
+        public async Task<IQueryable> FindBooksByPublisher(int publisherId)
+        {
+            var books = _repo.FindBooksByPublisher(publisherId);
+            return books;
+        }
+        public async Task<BookOprationResult> AddBook(AddBookDTO dto)
+        {
+            var book = _repo.Add(dto);
+            if(book != null)
+            {
+                return BookOprationResult.Success(message:"Book added successfully !",request:book);
+            }
+            else
+            {
+                return BookOprationResult.Failure("somthing went wrong");
+            }
+        }
+        public async Task<BookOprationResult> DeleteBook(int bookId)
+        {
+            var book = _repo.FindBook(bookId);
+            if(book != null)
+            {
+                _repo.Delete(bookId);
+                return BookOprationResult.Success(message:"Book deleted successfully !",request:book);
+            }
+            else
+            {
+                return BookOprationResult.Failure("somthing went wrong");
+            }
+        }
+
+        public async Task<BookOprationResult> DeactiveBook(int bookId)
+        {
+            var book = _repo.FindBook(bookId);
+            if(book != null)
+            {
+                _repo.DeactiveBook(bookId);
+                return BookOprationResult.Success(message:"Book deactivated successfully !",request:book);
+            }
+            else
+            {
+                return BookOprationResult.Failure("somthing went wrong");
+            }
+        }
+
+        public async Task<BookOprationResult> ActiveBook(int bookId)
+        {
+            var book = _repo.FindBook(bookId);
+            if(book != null)
+            {
+                _repo.ActiveBook(bookId);
+                return BookOprationResult.Success(message:"Book activated successfully !",request:book);
+            }
+            else
+            {
+                return BookOprationResult.Failure("somthing went wrong");
+            }
+        }
+
+
     }
 }
