74c0f9d891f4e4b186a86fbe1f73973a426e710f
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-04-25 11:18:23 +0330

    - booklend dto added

---
+++ Back-End/Application/DTOs/Lend/BookLendDTO.cs
@@ -0,0 +1,8 @@
+namespace Application.DTOs.Lend
+{
+    public class BookLendDTO
+    {
+        public int BookId { get; set; }
+        public int LnedId { get; set; }
+    }
+}
--- Back-End/Domain/Entities/BookLend.cs
+++ Back-End/Domain/Entities/BookLend.cs
@@ -1,4 +1,6 @@
 namespace Domain.Entities
 {
-    
+    public int Id {get; set;}
+    public int BookId {get; set;}
+    public int LendId {get; set;}
 }
